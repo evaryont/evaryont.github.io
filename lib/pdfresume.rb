e1d71b348379e82a1ebb387296d55ab8284b8b9b
Colin Shea <colin@evaryont.me>
2015-07-21 01:49:57 +0000

    output the command used in PDFKit

--- lib/pdfresume.rb
+++ lib/pdfresume.rb
@@ -5,23 +5,27 @@ class PDFResume < Middleman::Extension
 
   def initialize(app, options_hash={}, &block)
     super
+
+    input_file = 'build/resume.html'
+    output_file = 'build/resume.pdf'
+
     app.after_build do |builder|
       begin
-        kit = PDFKit.new(File.new('build/resume.html'),
-                         :margin_top => 10,
-                         :margin_bottom => 0,
-                         :margin_left => 0,
-                         :margin_right => 0,
-                         :print_media_type => true,
-                         :dpi => 96)
+        kit = PDFKit.new(File.new(input_file),
+                         print_media_type: true,
+                         enable_local_file_access: true,
+                         images: true,
+                         enable_javascript: true,
+                         dpi: 96)
 
-        file = kit.to_file('build/resume.pdf')
+        builder.say_status 'PDF Resume',  "Running #{kit.command(output_file)}..."
+        file = kit.to_file(output_file)
 
-      rescue Exception =>e
-        builder.say_status "PDF Resume",  "Error: #{e.message}", Thor::Shell::Color::RED
+      rescue Exception => e
+        builder.say_status 'PDF Resume',  "Error: #{e.message}", Thor::Shell::Color::RED
         raise
       end
-      builder.say_status "PDF Resume",  "PDF file available at build/resume.pdf"
+      builder.say_status 'PDF Resume',  "PDF file available at #{output_file}"
     end
   end
 
